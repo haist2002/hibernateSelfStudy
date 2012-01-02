1ef2ff013a5097aadd75d66a82425e17c61a3de7
안병휘 <haist2002@gmail.com>
2012-01-02 23:15:23 +0900

    테스트 추가

---
+++ src/test/java/TestMessage.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: haist2002
+ * Date: 12. 1. 2.
+ * Time: 오후 11:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestMessage {
+}
