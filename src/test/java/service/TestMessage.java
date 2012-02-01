7eb00f5517d90b6f31e3cf2234d0e6faff6b7fa8
안병휘 <haist2002@gmail.com>
2012-02-02 00:13:41 +0900

    공부 진행중

--- src/test/java/service/TestMessage.java
+++ src/test/java/service/TestMessage.java
@@ -1,5 +1,10 @@
 package service;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * Created by IntelliJ IDEA.
  * User: haist2002
@@ -7,7 +12,13 @@ package service;
  * Time: 오후 11:14
  * To change this template use File | Settings | File Templates.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:web/WEB-INF/spring/applicationContext.xml"})
 public class TestMessage {
 
+    @Test
+    public void testMessage() throws Exception {
 
+    }
 }
