481f54c37a924d21ab26e38a46e50067718e79c2
AnByeongHwi <haist2002@gmail.com>
2012-02-07 22:42:53 +0900

    새로운 브런치 등록

---
+++ src/test/java/service/TestMessageBox.java
@@ -0,0 +1,11 @@
+package service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12. 2. 7
+ * Time: 오후 10:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestMessageBox {
+}
