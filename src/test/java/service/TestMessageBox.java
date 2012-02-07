8bc42cc59a0b942e01245e0afad6c694095615ab
AnByeongHwi <haist2002@gmail.com>
2012-02-07 22:50:32 +0900

    테스트 분리

--- src/test/java/service/TestMessageBox.java
+++ src/test/java/service/TestMessageBox.java
@@ -1,6 +1,14 @@
 package service;
 
+import hello.domain.MessageBox;
+import hello.service.MessageBoxService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,10 +17,19 @@ import org.junit.Test;
  * Time: 오후 10:42
  * To change this template use File | Settings | File Templates.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:web/WEB-INF/spring/applicationContext.xml"})
 public class TestMessageBox {
 
+    @Autowired
+    private MessageBoxService messageBoxService;
+
     @Test
-    public void testMessageBox_생성과삭제() throws Exception {
-        //To change body of created methods use File | Settings | File Templates.
+    public void test_메시지박스_생성_후_저장() throws Exception {
+        MessageBox messageBox = new MessageBox("메모");
+        messageBoxService.saveMessageBox(messageBox);
+
+        assertEquals(messageBoxService.getMessageBox("메모").getLabel(),"메모");
+
     }
 }
