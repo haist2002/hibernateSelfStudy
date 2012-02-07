6083cb52b8d41816c7c3bd9a9ef2aef9fdb9da60
안병휘 <haist2002@gmail.com>
2012-02-07 13:51:16 +0900

    jpa 테스트 생성

---
+++ src/test/java/service/TestJpaMessage.java
@@ -0,0 +1,17 @@
+package service;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: haist2002
+ * Date: 12. 2. 7.
+ * Time: 오후 1:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestJpaMessage {
+    @Test
+    public void testMessae_메시지_생성후_저장() throws Exception {
+
+    }
+}
